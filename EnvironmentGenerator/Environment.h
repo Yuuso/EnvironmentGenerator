b5c191f78665c46424bc2bf0857669fc6bc17747
Juuso Turunen <juusoturunen94@hotmail.com>
2016-05-09 02:11:16 +0300

    Reset

--- EnvironmentGenerator/Environment.h
+++ EnvironmentGenerator/Environment.h
@@ -6,17 +6,6 @@
 #include <SpehsEngine/RNG.h>
 
 
-struct SectorPosition
-{
-	Sector major0; //4096x4096 majors
-	Sector major1; //4096x4096 majors
-	Sector major2; //4096x4096 majors
-	Sector major3; //4096x4096 minors
-
-	Sector minor0; //256x256 minors
-	Sector minor1; //current
-};
-
 class Environment
 {
 public:
--- EnvironmentGenerator/Sector.h
+++ EnvironmentGenerator/Sector.h
@@ -4,13 +4,10 @@
 #include <stdint.h>
 #include <vector>
 
-#define MAJOR_SIZE 4096
-#define MINOR_SIZE 256
-
 
 typedef uint8_t SectorDataType;
 
-namespace spehs{ class Polygon; }
+namespace spehs{ class Polygon; class WorldPosition; }
 
 struct SECTORDATA
 {
@@ -19,37 +16,18 @@ struct SECTORDATA
 	SectorDataType technology;
 };
 
-enum SECTORTYPE : bool
-{
-	MAJOR = true,
-	MINOR = false
-};
-
 class Sector
 {
 public:
 	Sector();
-	Sector(Sector* _parent, const uint16_t _x, const uint16_t _y, const SECTORTYPE _sectorbuild, const uint8_t _layer);
+	Sector(const spehs::WorldPosition& _position);
 	~Sector();
-
-	void buildSector(Sector* _parent, const uint16_t _x, const uint16_t _y, const SECTORTYPE _sectorbuild, const uint8_t _layer);
-	SECTORDATA getData(const uint16_t _x, const uint16_t _y);
-	void renderVisuals(const bool _state, const unsigned char _alpha = 255);
-
+	
 private:
-	void buildData(const SECTORDATA _parentData, const uint16_t _size);
-	void clearData();
-	void clearVisuals();
-
 	uint32_t localSeed;
-	uint16_t factor;
-	SECTORTYPE type;
-	SECTORDATA** rawdata;
-
-	Sector* parent;
-	uint16_t xInP, yInP;
-
-	std::vector<spehs::Polygon*>* primaryVisuals;
-	std::vector<spehs::Polygon*>* secondaryVisuals;
+	SECTORDATA rawdata;
+	
+	std::vector<spehs::Polygon*> primaryVisuals;
+	std::vector<spehs::Polygon*> secondaryVisuals;
 };
 
